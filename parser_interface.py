7b43be48f0c73206378323a7d4e5829081e27cf8
andeleyev <andy.datchev@gmail.com>
2024-12-11 15:02:09 +0000

    fixed reset and input keys

--- parser_interface.py
+++ parser_interface.py
@@ -6,6 +6,7 @@ import yaml
 from yaml.loader import SafeLoader
 from streamlit_extras.stylable_container import stylable_container
 import os
+from streamlit_extras.bottom_container import bottom
 
 st.set_page_config(page_title="SOP Form Parser", page_icon="🧪", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
@@ -38,15 +39,17 @@ def get_teacher_data(username=None, id=None):
         print(id)
         teacher = teachers.loc[teachers['username'] == username]
     print("Teacher: ", teacher)
+
+    st.session_state['ti_xp'] = str(teacher['work_experience'].values[0])
     return teacher
 
-# @st.cache_resource
+@st.cache_resource
 def get_student_data(id):
     global parser
     students = parser.students
 
     student = students.loc[students['id'] == int(id)]
-    print(student)
+    # print(student)
     if not student.empty:
         st.session_state['ti_age'] = str(student['age'].values[0])
         st.session_state['ti_profile'] = str(student['profile'].values[0])
@@ -76,13 +79,11 @@ if not "counter" in st.session_state:
 #    st.session_state["ti_teacher_id"] = ""
 if "ti_student_id" not in st.session_state:
     st.session_state["ti_student_id"] = ""
-if "date_i" not in st.session_state:
-    st.session_state["date_i"] = "today"
-if "text_sit" not in st.session_state:
+if "text_sit_def" not in st.session_state:
     st.session_state["text_sit"] = ""
-if "text_act" not in st.session_state:
+if "text_act_def" not in st.session_state:
     st.session_state["text_act"] = ""
-if "text_eff" not in st.session_state:
+if "text_eff_def" not in st.session_state:
     st.session_state["text_eff"] = ""
 if "grade_slider" not in st.session_state:
     st.session_state["grade_slider"] = 5
@@ -102,27 +103,50 @@ if "audi_eff" not in st.session_state:
     st.session_state["audi_eff_key"] = "eff" + str(st.session_state['counter'])
 
 
-def reset_state(test):
+def reset_state():
     print("resetting !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    test = "blabla"
     st.session_state.counter += 1
     st.session_state["audi_eff_key"] = "eff" + str(st.session_state['counter'])
     st.session_state["audi_act_key"] = "act" + str(st.session_state['counter'])
     st.session_state["audi_sit_key"] = "sit" + str(st.session_state['counter'])
-    st.session_state["ti_teacher_id"] = ""
-    st.session_state["ti_student_id"] = ""
-    st.session_state["date_i"] = "today"
+
+    st.session_state["tid"] = ""
+    st.session_state["sid"] = ""
+
     st.session_state["text_sit"] = ""
     st.session_state["text_act"] = ""
     st.session_state["text_eff"] = ""
-    st.session_state["grade_slider"] = 5
+    st.session_state["grade"] = 3
+
     st.session_state["ti_xp"] = ""
     st.session_state["ti_age"] = ""
     st.session_state["ti_xp_toghether"] = ""
     st.session_state["ti_profile"] = ""
+
     st.session_state["audi_sit"] = ""
     st.session_state["audi_act"] = ""
     st.session_state["audi_eff"] = ""
+
+
+if not "download" in st.session_state:
+    st.session_state.download = False
+
+if st.session_state['download']:
+    with bottom():
+        d1, d2 = st.columns(2)
+
+        with d1:
+            downloaded = st.download_button("изтегляне на попълнения формуляр", use_container_width=True, data=st.session_state.exel, file_name="filled_form.xlsx")
+        
+        with d2:
+            reset = st.button("ресет", use_container_width=True)
+
+
+        if downloaded or reset:
+            st.session_state.download = False
+            print("resetingggg")
+            reset_state()
+            st.rerun()
 
 
 authenticator.login(fields={'Form name':'Login', 'Username':'вашето потребителско име', 'Password':'парола', 'Login':'Login', 'Captcha':'Captcha'})
@@ -136,8 +160,8 @@ if st.session_state['authentication_status']:
     logged_teacher_id = str(logged_teacher['id'].values[0])
     
     
-    if "ti_teacher_id" not in st.session_state:
-        st.session_state["ti_teacher_id"] = logged_teacher_id
+    if "tid" not in st.session_state:
+        st.session_state["tid"] = logged_teacher_id
     # Title of the application
     # '# Въпросник "Ситуации соп" '
     '# Бланка за описване на ситуация и реакция при работа с деца със СОП'
@@ -165,26 +189,18 @@ if st.session_state['authentication_status']:
         return script, audio_path
 
     # Form layout
-    "## Идентификационна информация"
-    " "
-    "##### Вашият уникален анонимен идентификатор:"
-    st.session_state["ti_teacher_id"] = st.text_input("a", value=st.session_state["ti_teacher_id"], label_visibility="collapsed")
-    if st.session_state["ti_teacher_id"] != "":
-        teacher_form = get_teacher_data(id=st.session_state["ti_teacher_id"])
-        st.session_state['ti_xp'] = teacher_form['work_experience'].values[0]
-
+    st.markdown("## Идентификационна информация\n\n ##### Вашият уникален анонимен идентификатор:")
+    st.text_input("a", key="tid", label_visibility="collapsed")
+    if st.session_state["tid"] != "" and int(st.session_state["tid"]) != logged_teacher_id:
+        teacher_form = get_teacher_data(id=st.session_state["tid"])
 
     st.markdown("##### Уникален анонимен номер на детето:")
-    #print(st.session_state["ti_student_id"])
-    test = st.text_input("b", value=st.session_state["ti_student_id"], label_visibility="collapsed")
-    #print(st.session_state["ti_student_id"])
-    if test:
-        print("check")
-        st.session_state["ti_student_id"] = test
-        student = get_student_data(st.session_state["ti_student_id"])
+    st.text_input("b", key="sid", label_visibility="collapsed")
+    if st.session_state["sid"]:
+        student = get_student_data(int(st.session_state["sid"]))
 
     "##### Дата на случката:"   
-    date = st.date_input("Дата на случката:", value=st.session_state["date_i"], format="DD.MM.YYYY", label_visibility="collapsed") # remove default to make it today
+    date = st.date_input("Дата на случката:", format="DD.MM.YYYY", label_visibility="collapsed") # remove default to make it today
 
 
     "## Ситуация и реакция "
@@ -210,7 +226,7 @@ if st.session_state['authentication_status']:
         if transcript_sit != "":
             st.session_state["text_sit"] = transcript_sit
     with a1:
-        st.session_state["text_sit"] = st.text_area("ситуацията", value=st.session_state["text_sit"], placeholder="write here", height=257, label_visibility="collapsed")
+        st.text_area("ситуацията", key="text_sit", placeholder="write here", height=257, label_visibility="collapsed")
 
     "  "
     "#### **Опишете устно или писмено Вашата реакция:**"
@@ -232,7 +248,7 @@ if st.session_state['authentication_status']:
         if transcript_act != "":
             st.session_state["text_act"] = transcript_act
     with b1:
-        st.session_state["text_act"] = st.text_area("реакцията", value=st.session_state["text_act"], height=257, label_visibility="collapsed")
+        st.text_area("реакцията", key="text_act", height=257, label_visibility="collapsed")
 
     "  "
     "#### Опишете устно или писмено **ефекта** от Вашата реакция:"
@@ -254,26 +270,23 @@ if st.session_state['authentication_status']:
         if transcript_eff != "":
             st.session_state["text_eff"] = transcript_eff
     with c1:
-        st.session_state["text_eff"]= st.text_area("Опишете ефекта от вашата реакция:", value=st.session_state["text_eff"], height=257, label_visibility="collapsed")
+        st.text_area("Опишете ефекта от вашата реакция:", key="text_eff", height=257, label_visibility="collapsed")
 
     "#### Оценете ефекта от вашата реакция от 1 до 10:"
-    st.session_state["grade_slider"] = st.slider("**Оценете ефекта от вашата реакция от 1 до 10:**", 1, 10, value=st.session_state["grade_slider"], key="slider", label_visibility="collapsed")
+    st.slider("**Оценете ефекта от вашата реакция от 1 до 10:**", 1, 5, key="slider", label_visibility="collapsed")
 
-    "## Информация за специалиста и детето"
-    "#### Колко години опит имате:"
-    
-    st.session_state['ti_xp'] = st.text_input("Колко години опит имате:", value=st.session_state["ti_xp"], label_visibility="collapsed")
-    
+    st.markdown("## Информация за специалиста и детето \n\n #### Колко години опит имате:")
+    st.text_input("Колко години опит имате:", key="ti_xp", label_visibility="collapsed")
 
-    "#### На колко години е детето:"
-    
-    st.session_state["ti_age"] = st.text_input("На колко години е детето:", value=st.session_state["ti_age"], label_visibility="collapsed")
+    st.markdown("#### На колко години е детето:")
+    st.text_input("На колко години е детето:", key="ti_age", label_visibility="collapsed")
 
-    "#### От колко години работите с това дете:"
-    st.session_state["ti_xp_toghether"] = st.text_input("От колко години работите с това дете:", value=st.session_state["ti_xp_toghether"], label_visibility="collapsed")
 
-    "#### Какъв е профилът на детето? / Какъв е линкът към досието му:"
-    st.session_state["ti_profile"] = st.text_input("профилът", value=st.session_state["ti_profile"], label_visibility="collapsed")
+    st.markdown("#### От колко години работите с това дете:")
+    st.text_input("От колко години работите с това дете:", key="ti_xp_toghether", label_visibility="collapsed")
+
+    st.markdown("#### Какъв е профилът на детето? / Какъв е линкът към досието му:")
+    st.text_input("профилът", key="ti_profile", label_visibility="collapsed")
 
     st.markdown(" ")
 
@@ -289,18 +302,15 @@ if st.session_state['authentication_status']:
     ):  
         submit = st.button("подаване на формуляр", use_container_width=True)
 
-        if not "download" in st.session_state:
-            st.session_state.download = False
-
-        if submit or st.session_state['download']:
+        if submit:
 
             
             st.session_state.download = True
 
             if submit:
                 inputs = {
-                    "teacher_id": st.session_state["ti_teacher_id"],
-                    "student_id": st.session_state["ti_student_id"],
+                    "teacher_id": st.session_state["tid"],
+                    "student_id": st.session_state["sid"],
                     "date": date.strftime("%d-%m-%Y"),
                     "teacher_xp": st.session_state["ti_xp"],
                     "student_age": st.session_state["ti_age"],
@@ -309,7 +319,7 @@ if st.session_state['authentication_status']:
                     "situation": st.session_state["text_sit"],
                     "action": st.session_state["text_act"],
                     "effect": st.session_state["text_eff"],
-                    "grade": st.session_state["grade_slider"],
+                    "grade": st.session_state['slider']
                 }
         
                 exel, exel_path = parser.write_to_exel(inputs)
@@ -324,24 +334,10 @@ if st.session_state['authentication_status']:
                 if transcript_eff == "":
                     transcript_eff = "empty"
 
-                parser.add_from_to_db(exel_path, st.session_state["ti_teacher_id"], audio_sit_path, audio_act_path, audio_eff_path, 
+                parser.add_from_to_db(exel_path, st.session_state["tid"], audio_sit_path, audio_act_path, audio_eff_path, 
                                       transcript_sit, transcript_act, transcript_eff, st.session_state["ti_student_id"], date.strftime("%d-%m-%Y"), time)
 
-            d1, d2 = st.columns(2)
-
-            with d1:
-                downloaded = st.download_button("изтегляне на попълнения формуляр", use_container_width=True, data=st.session_state.exel, file_name="filled_form.xlsx")
-            
-            with d2:
-                reset = st.button("ресет", use_container_width=True)
-
-
-            if downloaded or reset:
-                st.session_state.download = False
-                print("resetingggg")
-                reset_state(test)
                 st.rerun()
-                
 
 
 elif st.session_state['authentication_status'] is False:
