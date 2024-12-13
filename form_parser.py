2b73f8df31f388319ccdb7547d11a24a4960a1b3
andeleyev <andy.datchev@gmail.com>
2024-12-13 13:47:52 +0000

    switch do google sheet isntead of local csv

--- form_parser.py
+++ form_parser.py
@@ -7,6 +7,31 @@ import openai
 import pandas as pd
 from streamlit_authenticator import Hasher
 import yaml
+import re
+
+
+import gdown
+from bs4 import BeautifulSoup
+import gspread
+from google.oauth2.service_account import Credentials
+import pandas as pd
+import json
+import streamlit as st
+from googleapiclient.discovery import build
+
+
+# in secrets.toml service account
+SERVICE_ACC_INFO = json.loads(st.secrets["google_credentials"]["json_content"])
+
+# Define the scope for the APIs
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/drive.file"
+]
+
+# Authenticate
+credentials = Credentials.from_service_account_info(SERVICE_ACC_INFO, scopes=SCOPES)
 
 # Define the field in the form where the inputs are
 teacher_id = 'B6'
@@ -26,8 +51,7 @@ rating = 'B22'
 
 class Parser():
 
-    def __init__(self, logging = True, logging_path = "logging", exel_template = "form.xlsx", transcriptor = "api",
-                 db_teacher_path = os.path.join("data", "teacher.csv"), db_students_path = os.path.join("data", "student.csv"), db_forms_path = os.path.join("data", "forms_meta.csv")):
+    def __init__(self, logging = True, exel_template = "form.xlsx", transcriptor = "api"):
 
         if transcriptor == "api":
             self.model = openai.OpenAI()
@@ -36,24 +60,27 @@ class Parser():
 
         self.transcriptor = transcriptor
 
-        self.teachers = pd.read_csv(db_teacher_path)
-        self.students = pd.read_csv(db_students_path)
-        self.forms_meta_path = db_forms_path
-        self.forms_meta = pd.read_csv(db_forms_path)
+        client =  gspread.authorize(credentials)
+        self.client = client
+        
+        sheet_meta = client.open_by_key("1Rqw51AqqepG78FmrN1UqiWvwPS5ITpzSCA1Bx4WDmkU").sheet1
+        data_meta = sheet_meta.get_all_values()
+        self.forms_meta =pd.DataFrame(data_meta[1:], columns=data_meta[0])
+
+        sheet_teacher = client.open_by_key("1l44B3aAaLWZhJOtWDjWkCLz89wTTk71fjai8_URV68k").sheet1
+        data_teacher = sheet_teacher.get_all_values()
+        self.teachers = pd.DataFrame(data_teacher[1:], columns=data_teacher[0])
+        
+        sheeet_student = client.open_by_key("1BcsZPL-CKhOmBYWc96cuK594Zau1anQhdaBCQTUAsHU").sheet1
+        data_student = sheeet_student.get_all_values()
+        self.students = pd.DataFrame(data_student[1:], columns=data_student[0])
 
         self.logging = logging
-        self.logging_path = logging_path
-        self.voice_loggs = os.path.join(logging_path, "recordings")
-        self.transcript_loggs = os.path.join(logging_path, "transcripts")
+        self.voice_loggs = "17sNd1INEALhXbEiA5jklEu68SoZverUm"
+        self.transcript_loggs = "1Axk6NkCyEUTcQWT_ldPPi3UoStEifQlL"
 
-        self.forms = os.path.join(logging_path, "forms")
+        self.forms = "forms"
         self.exel_template = exel_template
-
-        if self.logging:
-            if not os.path.isdir(self.logging_path):
-                os.makedirs(self.voice_loggs)
-                os.makedirs(self.forms)
-                os.makedirs(self.transcript_loggs)
 
         self.parse_to_yaml()
 
@@ -66,14 +93,19 @@ class Parser():
 
         # Create the credentials dictionary
         credentials = {"usernames": {}}
-        for _, row in df.iterrows():
+        for i, row in df.iterrows():
+            names = str(row['Име на учител']).split(" ")
+            if len(names) >= 2:
+                first, second = names[0], names[1] 
+            else: 
+                first, second = row['Име на учител'], " - "
             user_id = str(row["username"])
             credentials["usernames"][user_id] = {
-                "email": row["email"],
-                "first_name": row["first_name"],
-                "last_name": row["last_name"],
+                "email": row["Електронна Поща"],
+                "first_name": first,
+                "last_name": second,
                 "logged_in": False,
-                "password": row["password"],
+                "password": row['pwd'],
             }
 
         # Create the YAML structure
@@ -144,7 +176,7 @@ class Parser():
 
         return data, scenario
 
-    def dict_to_wb(self, wb_template, save_path, inputs):
+    def dict_to_wb(self, wb_template, inputs):
         workbook = openpyxl.load_workbook(wb_template)
         # Select the active worksheet
         sheet = workbook.active
@@ -160,16 +192,14 @@ class Parser():
         sheet[action_taken] = inputs['action']
         sheet[effect] = inputs['effect']
         sheet[rating] = inputs['grade']
-        
-        workbook.save(save_path)
 
         buffer = io.BytesIO()
         workbook.save(buffer)
         buffer.seek(0)
         return buffer
 
-    def write_to_exel(self, inputs, exel_template_path=""):
-
+    def create_exel(self, inputs, exel_template_path=""):
+        # Gets all field for the form.xlsx and return a workbook
         if self.is_valid_template(exel_template_path):
             template = exel_template_path
         elif self.is_valid_template(self.exel_template):
@@ -178,10 +208,25 @@ class Parser():
             print("Invalid template - Nothing will be done")
             return
         
-        path = self._get_unused_name(self.forms, template)
-        print("Saving form: ", path)
-        return self.dict_to_wb(template, path, inputs), path
+        return self.dict_to_wb(template, inputs)
 
+    def extract_file_id(self, url):
+        # Regular expression for Google Drive file ID
+        file_id_pattern = r"(?:/d/|id=|open\?id=|uc\?id=)([a-zA-Z0-9_-]+)"
+        match = re.search(file_id_pattern, url)
+        if match:
+            return match.group(1)
+        else:
+            raise ValueError("Invalid Google Drive URL")
+
+    def get_filename(self, url):
+        with build("drive", "v3", credentials=credentials) as service:
+            file_id = self.extract_file_id(url)
+            print(file_id)
+
+            file_metadata = service.files().get(fileId=file_id, fields='name').execute()
+
+        return file_metadata['name']
 
     def _get_unused_name(self, folder, name):
             dir = os.listdir(folder)
@@ -194,13 +239,6 @@ class Parser():
                 path = file_name + f"({i})" + extension
                 if not path in dir:
                     return os.path.join(folder, path)
-
-    def existing_teacher(self, id):
-        return  id in self.dynamic_db['id'].values
-    
-    def existing_student(self, id):
-        return id in self.dynamic_db['id'].values
-
 
     def add_from_to_db(self, from_path, t_id, audio_sit, audio_act, audio_eff, script_sit, script_act, script_eff,  s_id, date, time):
         # row = form_id,from_path,date,time,audio_sit,audio_action,audio_effect,transcript_sit,transcript_action,transcript_effect,teacher_id,student_id
--- parser_interface.py
+++ parser_interface.py
@@ -8,6 +8,8 @@ from streamlit_extras.stylable_container import stylable_container
 import os
 from streamlit_extras.bottom_container import bottom
 from st_audiorec import st_audiorec
+import gdown
+from bs4 import BeautifulSoup
 
 st.set_page_config(page_title="SOP Form Parser", page_icon="🗣️", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
@@ -21,49 +23,111 @@ today = now.strftime("%d-%m-%Y")
 def initialize(key):
     os.environ["OPENAI_API_KEY"] = key
     parser = form_parser.Parser()
-    return parser
+    students = parser.students
+    ids = students['Идентификационен номер'].values
+    return parser, ids
 
-parser = initialize(api)
+parser, student_ids = initialize(api)
+
+print(student_ids)
 
 @st.cache_resource
-def get_teacher_data(username=None, id=None):
+def get_teacher_data(username):
     global parser
     teachers = parser.teachers
-    # print(teachers)
-    if username == None and id == None:
-        print("No username or id were provided to get teacher")
-        return
 
-    if username is None:
-        teacher = teachers.loc[teachers["id"] == int(id)]
-    else:
-        print(id)
-        teacher = teachers.loc[teachers['username'] == username]
+    teacher = teachers.loc[teachers['username'] == username]
     print("Teacher: ", teacher)
 
-    st.session_state['ti_xp'] = str(teacher['work_experience'].values[0])
-    return teacher
+    if not teacher.empty:
+        diff = now.year - int(teacher['Ресурсен учител от година'].values[0])
+        teacher_xp = f"{diff}-{diff + 1}"
+        teacher_id = teacher['Идентификационен номер на учител'].values[0]
+    else:
+        st.warning("Did not find the teacher in the database")
+
+    return teacher_id, teacher_xp, 
+
+def extract_file_name_from_html(url):
+    output = 'test.html'
+    global parser
+    parser.download(url, output)
+
+    try:
+        with open(output, 'r', encoding='utf-8') as file:
+            content = file.read()
+            
+        # Parse the HTML content
+        soup = BeautifulSoup(content, 'html.parser')
+
+        # Find the <meta> tag with property "og:title"
+        meta_tag = soup.find('meta', property='og:title')
+
+        if meta_tag and 'content' in meta_tag.attrs:
+            return meta_tag['content']
+
+        return None
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
 @st.cache_resource
 def get_student_data(id):
     global parser
     students = parser.students
-
-    student = students.loc[students['id'] == int(id)]
-    # print(student)
+    print(students)
+    student = students.loc[students['Идентификационен номер'] == str(id)]
+    print(student)
     if not student.empty:
-        st.session_state['ti_age'] = str(student['age'].values[0])
-        st.session_state['ti_profile'] = str(student['profile'].values[0])
+        url = str(student['линк към файл с профил'].values[0])
+        print(url)
+        st.session_state['ti_profile'] = url
+        file = parser.get_filename(url)
+        if file == None:
+            file = "link"
+        st.session_state['profile_filename'] = file
+
+        autism = bool(int(student['аутизъм'].values[0]))
+        dislexia = bool(int(student['дислекция'].values[0]))
+        ns1 = bool(int(student['нз1'].values[0]))
+        ns2 = bool(int(student['нз2'].values[0]))
+        ns3 = bool(int(student['нз3'].values[0]))
+        ns4 = bool(int(student['нз4'].values[0])) 
+        ns5 = bool(int(student['нз5'].values[0])) 
+        ns6 = bool(int(student['нз6'].values[0])) 
+        ns7 = bool(int(student['нз7'].values[0])) 
+        ns8 = bool(int(student['нз8'].values[0])) 
+        ns9 = bool(int(student['нз9'].values[0]))        
+        ns10 = bool(int(student['нз10'].values[0])) 
+        ns11 = bool(int(student['нз11'].values[0]))
+        ns12 = bool(int(student['нз12'].values[0])) 
+
+        st.session_state['autism'] = autism
+        st.session_state['dislexia'] = dislexia
+        st.session_state['ns1'] = ns1
+        st.session_state['ns2'] = ns2
+        st.session_state['ns3'] = ns3
+        st.session_state['ns4'] = ns4
+        st.session_state['ns5'] = ns5
+        st.session_state['ns6'] = ns6
+        st.session_state['ns7'] = ns7
+        st.session_state['ns8'] = ns8
+        st.session_state['ns9'] = ns9     
+        st.session_state['ns10'] = ns10
+        st.session_state['ns11'] = ns11
+        st.session_state['ns12'] = ns12
+
+        student_profile = [url, autism, dislexia, ns1, ns2, ns3, ns4, ns5, ns6, ns7, ns8, ns9, ns10, ns11, ns12]
     else:
         st.warning("Did not find the student in the database")
+        return None
 
-    return student
+    return student_profile, file
 
+# Login in Funitonality
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
-
-# Pre-hashing all plain text passwords once
-#hashed = stauth.Hasher.hash_passwords(config['credentials'])
 
 authenticator = stauth.Authenticate(
     config['credentials'],
@@ -73,37 +137,38 @@ authenticator = stauth.Authenticate(
     auto_hash=False
 )
 
+
+
 if not "counter" in st.session_state:
     st.session_state.counter = 0
 
 #if "ti_teacher_id" not in st.session_state: THis better later
 #    st.session_state["ti_teacher_id"] = ""
-if "ti_student_id" not in st.session_state:
-    st.session_state["ti_student_id"] = ""
+#if "profile_filename" not in st.session_state:
+#    st.session_state["profile_filename"] = ""
+
 if "text_sit" not in st.session_state:
     st.session_state["text_sit"] = ""
 if "text_act" not in st.session_state:
     st.session_state["text_act"] = ""
 if "text_eff" not in st.session_state:
     st.session_state["text_eff"] = ""
-if "grade_slider" not in st.session_state:
-    st.session_state["grade_slider"] = 5
-if "ti_xp" not in st.session_state:
-    st.session_state["ti_xp"] = ""   
-if "ti_age" not in st.session_state:
-    st.session_state["ti_age"] = ""
+
 if "ti_xp_toghether" not in st.session_state:
     st.session_state["ti_xp_toghether"] = ""
+
 if "ti_profile" not in st.session_state:
     st.session_state["ti_profile"] = ""
-if "audi_sit" not in st.session_state:
+
+if "audi_sit_keys" not in st.session_state:
     st.session_state["audi_sit_key"] = "sit" + str(st.session_state['counter'])
-if "audi_act" not in st.session_state:
+if "audi_act_key" not in st.session_state:
     st.session_state["audi_act_key"] = "act" + str(st.session_state['counter'])
-if "audi_eff" not in st.session_state:
+if "audi_eff_key" not in st.session_state:
     st.session_state["audi_eff_key"] = "eff" + str(st.session_state['counter'])
 
 
+    
 def reset_state():
     print("resetting !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     st.session_state.counter += 1
@@ -111,7 +176,6 @@ def reset_state():
     st.session_state["audi_act_key"] = "act" + str(st.session_state['counter'])
     st.session_state["audi_sit_key"] = "sit" + str(st.session_state['counter'])
 
-    # st.session_state["tid"] = ""
     st.session_state["sid"] = ""
 
     st.session_state["text_sit"] = ""
@@ -119,8 +183,6 @@ def reset_state():
     st.session_state["text_eff"] = ""
     st.session_state["grade"] = 3
 
-    # st.session_state["ti_xp"] = ""
-    st.session_state["ti_age"] = ""
     st.session_state["ti_xp_toghether"] = ""
     st.session_state["ti_profile"] = ""
 
@@ -151,23 +213,19 @@ if st.session_state['download']:
 authenticator.login(fields={'Form name':'Login', 'Username':'вашето потребителско име', 'Password':'парола', 'Login':'Login', 'Captcha':'Captcha'})
 
 if st.session_state['authentication_status']:
-    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.write(f'Добре дошли *{st.session_state["name"]}* ')
 
     username = st.session_state["username"]
+    teacher_id, teacher_xp = get_teacher_data(username)  
+
+    st.write(f"Вашият Идентификатор: {teacher_id}")
     
-    logged_teacher = get_teacher_data(username)  
-    logged_teacher_id = str(logged_teacher['id'].values[0])
-    
-    
-    if "tid" not in st.session_state:
-        st.session_state["tid"] = logged_teacher_id
     # Title of the application
     # '# Въпросник "Ситуации соп" '
     '# Бланка за описване на ситуация и реакция при работа с деца със СОП'
     # "###### Формулар за събиране на ситуации относно индивидуалните прояви и предизвикателства при деца с аутизъм"
 
     " "
-    teacher_form = logged_teacher
 
     # Speech input button
     # audio_st = st.audio_input("🎤 Говори", key=f"voice_input_{st.session_state.audio_key}")
@@ -188,22 +246,30 @@ if st.session_state['authentication_status']:
         return script, audio_path
 
     # Form layout
-    st.markdown("## Идентификационна информация\n\n ##### Вашият уникален анонимен идентификатор:")
-    st.text_input("a", key="tid", label_visibility="collapsed")
-    if st.session_state["tid"] != "" and int(st.session_state["tid"]) != logged_teacher_id:
-        teacher_form = get_teacher_data(id=st.session_state["tid"])
+    #st.markdown("## Идентификационна информация\n\n ##### Вашият уникален анонимен идентификатор:")
+    #st.text_input("a", key="tid", label_visibility="collapsed")
+    #if st.session_state["tid"] != "" and int(st.session_state["tid"]) != logged_teacher_id:
+    #    teacher_form = get_teacher_data(id=st.session_state["tid"])
 
     st.markdown("##### Уникален анонимен номер на детето:")
-    st.text_input("b", key="sid", label_visibility="collapsed")
+    student = st.selectbox("b", student_ids, key="sid", label_visibility="collapsed", index=None)
+    
     if st.session_state["sid"]:
-        student = get_student_data(int(st.session_state["sid"]))
+        student_pr, file = get_student_data(int(student))
+        new_student_pr = student_pr.copy()
+        if file == "link":
+            st.toast("The URL to the student card could not be loaded correctly", icon="❗")
 
-    "##### Дата на случката:"   
-    date = st.date_input("Дата на случката:", format="DD.MM.YYYY", label_visibility="collapsed") # remove default to make it today
+    st.markdown("#### От колко години работите с това дете:")
+    st.text_input("От колко години работите с това дете:", key="ti_xp_toghether", label_visibility="collapsed")
 
 
     "## Ситуация и реакция "
     "  "
+    "##### Дата на :rainbow[случката:]"   
+    date = st.date_input("Дата на :rainbow[случката]:", format="DD.MM.YYYY", label_visibility="collapsed") # remove default to make it today
+
+
     "#### **Опишете устно или писмено ситуацията, която се е случила:**"
     "(за гласов запис натиснете микрофона)"
     a1, a2 = st.columns([5, 1])
@@ -271,7 +337,7 @@ if st.session_state['authentication_status']:
     with c1:
         st.text_area("Опишете ефекта от вашата реакция:", key="text_eff", height=257, label_visibility="collapsed")
 
-    st.markdown("#### Оценете ефекта от вашата реакция от 1 до 5:")
+    st.markdown("#### Оценете ефекта от Вашата реакция:")
     #e1, e2 = st.columns([5, 1])
     #with e1:
     #    st.markdown("#### Оценете ефекта от вашата реакция от 1 до 10:")
@@ -286,74 +352,107 @@ if st.session_state['authentication_status']:
     4: ефективна
     5: много ефективна
     """
-    st.slider("(за повече информация натиснете въпросителния знак) ", 1, 5, key="slider", help=explain_grades, label_visibility="visible")
+    #st.slider("(за повече информация натиснете въпросителния знак) ", 1, 5, key="slider", help=explain_grades, label_visibility="visible")
+    grades=[1, 2, 3, 4, 5]
 
-    st.markdown("## Информация за специалиста и детето \n\n #### Колко години опит имате:")
-    st.text_input("Колко години опит имате:", key="ti_xp", label_visibility="collapsed")
+    def grade_to_label(g):
+        labels = ("лоша реакция", "неефективна реакция", "слабо ефективна реакция", "ефективна реакция", "много ефективна реакция")
+        return labels[g - 1]
 
-    st.markdown("#### На колко години е детето:")
-    st.text_input("На колко години е детето:", key="ti_age", label_visibility="collapsed")
+    grade = st.radio(
+        "How would you like to be contacted?", grades, format_func=grade_to_label, index=None, label_visibility="collapsed")
 
 
-    st.markdown("#### От колко години работите с това дете:")
-    st.text_input("От колко години работите с това дете:", key="ti_xp_toghether", label_visibility="collapsed")
+    st.markdown("#### Състояние на детето")
 
-    st.markdown("#### Какъв е профилът на детето? / Какъв е линкът към досието му:")
-    st.text_input("профилът", key="ti_profile", label_visibility="collapsed")
+    if not st.session_state['ti_profile'] == "":
 
-    st.markdown(" ")
+        f1, f2, f3 = st.columns(3)
 
-    submit = st.button("подаване на формуляр", type="primary", use_container_width=True)
+        with f1:
+            st.checkbox("Аутизъм (МКБ:ььь)", key="autism")
+            st.checkbox("нз2", key="ns2")
+            st.checkbox("нз5", key="ns5")
+            st.checkbox("нз8", key="ns8")
+            st.checkbox("нз11", key="ns11")
 
-    if not "ballons" in st.session_state:
-        st.session_state.balloons = False
+        with f2:
+            st.checkbox("Дислексия (МКБ:ььь)", key="dislexia")
+            st.checkbox("нз3", key="ns3")
+            st.checkbox("нз6", key="ns6")
+            st.checkbox("нз9", key="ns9")
+            st.checkbox("нз12", key="ns12")
 
-    if st.session_state.balloons:
-        st.session_state.balloons = False
-        st.balloons()
-        st.toast("Формулара е испратен", icon='🥳') 
+        with f3:
+            st.checkbox("нз1", key="ns1")
+            st.checkbox("нз4", key="ns4")
+            st.checkbox("нз7", key="ns7")
+            st.checkbox("нз10", key="ns10")
 
-    wav_audio = st_audiorec()
 
-    if wav_audio is not None:
-        st.audio(wav_audio, format="audio/wav")
+        st.markdown("#### Какъв е линкът към картата за функционална оценка на детето:")
+        #st.text_input("профилът", key="ti_profile", label_visibility="collapsed")
+        st.link_button(st.session_state['profile_filename'], st.session_state['ti_profile'])
 
-    if submit:
-        st.session_state.download = True
+        st.markdown(" ")
 
-        inputs = {
-            "teacher_id": st.session_state["tid"],
-            "student_id": st.session_state["sid"],
-            "date": date.strftime("%d-%m-%Y"),
-            "teacher_xp": st.session_state["ti_xp"],
-            "student_age": st.session_state["ti_age"],
-            "xp_with_child": st.session_state["ti_xp_toghether"],
-            "student_profile": st.session_state["ti_profile"],
-            "situation": st.session_state["text_sit"],
-            "action": st.session_state["text_act"],
-            "effect": st.session_state["text_eff"],
-            "grade": st.session_state['slider']
-        }
+        submit = st.button("подаване на формуляр", type="primary", use_container_width=True)
 
-        exel, exel_path = parser.write_to_exel(inputs)
-        if not exel is None:
+        if not "ballons" in st.session_state:
+            st.session_state.balloons = False
+
+        if st.session_state.balloons:
+            st.session_state.balloons = False
+            st.balloons()
+            st.toast("Формулярът е изпратен", icon='🥳') 
+
+        wav_audio = st_audiorec()
+
+        if wav_audio is not None:
+            st.audio(wav_audio, format="audio/wav")
+
+        if submit:
+            st.session_state.download = True
             st.session_state.balloons = True
-        st.session_state.exel = exel
-        # print(st.session_state.exel)
-        time = now.strftime('%H:%M')
 
-        if transcript_sit == "":
-            transcript_sit = "empty"
-        if transcript_act == "":
-            transcript_act = "empty"
-        if transcript_eff == "":
-            transcript_eff = "empty"
+            update = not new_student_pr == student_pr
+            print(update)
 
-        parser.add_from_to_db(exel_path, st.session_state["tid"], audio_sit_path, audio_act_path, audio_eff_path, 
-                                transcript_sit, transcript_act, transcript_eff, st.session_state["ti_student_id"], date.strftime("%d-%m-%Y"), time)
+            inputs = {
+                "teacher_id": teacher_id,
+                "student_id": st.session_state["sid"],
+                "date": date.strftime("%d-%m-%Y"),
+                "teacher_xp": st.session_state["ti_xp"],
+                "student_age": st.session_state["ti_age"],
+                "xp_with_child": st.session_state["ti_xp_toghether"],
+                "student_profile": st.session_state["ti_profile"],
+                "situation": st.session_state["text_sit"],
+                "action": st.session_state["text_act"],
+                "effect": st.session_state["text_eff"],
+                "grade": st.session_state['slider']
+            }
 
+            exel, exel_path = parser.write_to_exel(inputs)
+            if not exel is None:
+                st.session_state.balloons = True
+            st.session_state.exel = exel
+            # print(st.session_state.exel)
+            time = now.strftime('%H:%M')
+
+            if transcript_sit == "":
+                transcript_sit = "empty"
+            if transcript_act == "":
+                transcript_act = "empty"
+            if transcript_eff == "":
+                transcript_eff = "empty"
+
+            parser.add_from_to_db(exel_path, teacher_id, audio_sit_path, audio_act_path, audio_eff_path, 
+                                    transcript_sit, transcript_act, transcript_eff, st.session_state["ti_student_id"], date.strftime("%d-%m-%Y"), time)
+
+            st.rerun()
+    else:
+        st.warning("Моля изберете ученик")
         
-        st.rerun()
 
 
 elif st.session_state['authentication_status'] is False:
